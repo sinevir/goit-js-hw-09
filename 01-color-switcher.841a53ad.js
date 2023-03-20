1557f3faa2279773221e2b47a73dc74666855620
sinevir <o.s.dubbie@gmail.com>
2023-03-20 14:14:57 +0000

    Deploying to gh-pages from @ sinevir/goit-js-hw-09@656b015556b3a713b0b9572e25e72cc1a3f8cd74 🚀

---
+++ 01-color-switcher.841a53ad.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.disabled=!1;var a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.841a53ad.js.map
